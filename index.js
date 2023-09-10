82534a5e4c2126b60a75d7ea5db977435a387ca5
KevinYu1580 <kevin1580@gmail.com>
2023-09-10 18:07:54 +0800

    0910-1

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
 // 引入模組
-import pwdValidation from './pwdRegexValidation.js'
+import RegexValidator from './pwdRegexValidation.js'
 
 Vue.createApp({
   data() {
     return {
-      // 密碼規則陣列，格式為 [{Name: '密碼規則名稱', Regex: '密碼規則正規式'}]
-      pwdRuleList: [],
-      inputVal: '',
-      moduleOutput: false,
+      pwdRuleList: [], //正規式陣列
+      inputVal: '', //input輸入值
+      moduleOutput: false, //正規式模組輸出
     }
   },
   components: {
     // 註冊元件
-    pwdValidation,
+    RegexValidator,
   },
 
   methods: {
-    // 從後端取得密碼規則陣列
     getPwdRuleList() {
-      // 假設後端api的response為 @param:returnData，後端格式需與下方格式相同
-      let returnData = [
+      // 使用者自訂的密碼規則(格式須符合模組格式)
+      this.pwdRuleList = [
         { Name: '長度為8-20碼', Regex: '/^.{8,20}$/' },
         { Name: '含數字0-9', Regex: '/[0-9]/' },
         { Name: '含英文字大寫A-Z', Regex: '/[A-Z]/' },
         { Name: '含英文字小寫a-z', Regex: '/[a-z]/' },
         { Name: '特殊符號#?!@$%^&*-', Regex: '/[#?!@$%^&*-]/' },
       ]
-
-      //在取得的密碼規則陣列中，加入key: Passed，該key代表該正規式是否已符合
-      returnData.forEach((item, index) => {
-        returnData[index]['Passed'] = false
-      })
-      this.pwdRuleList = returnData //將密碼規則陣列記錄在Vue.data中，供chkPwd_module()使用
     },
+    // 接收正規式模組輸出
     updatePwdModuleOutput(output) {
       this.moduleOutput = output
     },
--- pwdRegexValidation.js
+++ pwdRegexValidation.js
@@ -4,7 +4,26 @@ export default {
       outPut_boolean: false, // true符合，false不符合
     }
   },
-  props: ['regexList', 'inputValue', 'ttt'],
+  props: {
+    regexList: {
+      type: Array,
+
+      // 如未傳入regexList，預設密碼規則為下
+      default: () => {
+        return [
+          { Name: '長度為8-20碼', Regex: '/^.{8,20}$/' },
+          { Name: '含數字0-9', Regex: '/[0-9]/' },
+          { Name: '含英文字大寫A-Z', Regex: '/[A-Z]/' },
+          { Name: '含英文字小寫a-z', Regex: '/[a-z]/' },
+          { Name: '特殊符號#?!@$%^&*-', Regex: '/[#?!@$%^&*-]/' },
+        ]
+      },
+    },
+    inputValue: {
+      type: String,
+      required: true,
+    },
+  },
   template: `
   <div class="pwdCheck">
   <div
@@ -12,25 +31,30 @@ export default {
     class="pwdCheck__item"
     :class="{'pwdCheck__item--passed': item.Passed}"
   >
-    <!-- svg -->
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="17"
-      height="18"
-      viewBox="0 0 17 18"
-      fill="none"
-    >
-      <path
-        d="M14.5832 8.42858V9.00358C14.5825 10.3513 14.1461 11.6627 13.3391 12.7422C12.5321 13.8217 11.3978 14.6114 10.1053 14.9935C8.81289 15.3756 7.43154 15.3298 6.16729 14.8627C4.90305 14.3956 3.82366 13.5324 3.0901 12.4017C2.35654 11.2711 2.00812 9.93361 2.0968 8.58877C2.18548 7.24393 2.70651 5.96378 3.58218 4.93925C4.45785 3.91471 5.64124 3.20069 6.95587 2.90367C8.27049 2.60665 9.64592 2.74254 10.877 3.29108M14.5832 4L8.33325 10.2562L6.45825 8.38125"
-        stroke="#9E9E9E"
-        stroke-width="1.25"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
+  <div class="item__svgWrapper">
+  <slot name="icon">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 18 18"
+    fill="none"
+  >
+    <path
+      d="M14.5832 8.42858V9.00358C14.5825 10.3513 14.1461 11.6627 13.3391 12.7422C12.5321 13.8217 11.3978 14.6114 10.1053 14.9935C8.81289 15.3756 7.43154 15.3298 6.16729 14.8627C4.90305 14.3956 3.82366 13.5324 3.0901 12.4017C2.35654 11.2711 2.00812 9.93361 2.0968 8.58877C2.18548 7.24393 2.70651 5.96378 3.58218 4.93925C4.45785 3.91471 5.64124 3.20069 6.95587 2.90367C8.27049 2.60665 9.64592 2.74254 10.877 3.29108M14.5832 4L8.33325 10.2562L6.45825 8.38125"
+      stroke="#9E9E9E"
+      stroke-width="1.25"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+  </slot>
+  
+  </div>
+
     <!-- 正規式名稱 -->
     <span>{{item.Name}}</span>
+    
   </div>
+  
   </div>
         `,
   methods: {
@@ -39,31 +63,25 @@ export default {
       const RegexObj = string.match(/\/(.+)\/.*/)[1]
       return new RegExp(RegexObj)
     },
+    // 驗證
     async validate(value) {
-      //重置驗證結果
-      this.outPut_boolean = false
+      // 已通過的正規式數量
+      let passedItemLength = 0
 
-      for (let i = 0; i < this.regexList.length; i++) {
-        let regex = this.stringToRegex(this.regexList[i].Regex)
+      this.regexList.forEach((item, index) => {
+        // 取得正規式物件
+        let regex = this.stringToRegex(this.regexList[index].Regex)
 
-        // 檢查是否符合正規式，並將結果記錄在regexList[i].Passed，以控制template UI
-        if (regex.test(value)) {
-          //如果該正規式符合，則將key: Passed = true
-          this.regexList[i].Passed = true
-        } else {
-          //如果該正規式不符合，則將key: Passed = false
-          this.regexList[i].Passed = false
-        }
+        // 檢查是否符合正規式，並執行以下
+        // 1: 將結果記錄在regexList.Passed，以控制template UI樣式，如果該正規式符合，則將key: Passed = true，反之key: Passed = false
+        // 2: passedItemLength++，用來計算已通過的正規式數量，如果該正規式符合，則passedItemLength+1, 反之passedItemLength不變
+        regex.test(value)
+          ? ((this.regexList[index].Passed = true), passedItemLength++)
+          : (this.regexList[index].Passed = false)
+      })
 
-        // 如果有一個正規式不符合，則將outPut_boolean = false
-        if (this.regexList[i].Passed === false) {
-          this.outPut_boolean = false
-          continue
-        } else {
-          // 如果全部正規式都符合，則將outPut_boolean = true
-          this.outPut_boolean = true
-        }
-      }
+      // 如果已通過的正規式數量等於正規式陣列長度，代表全部符合，將output設為true，反之設為false
+      this.outPut_boolean = passedItemLength == this.regexList.length ? true : false
     },
     sendOutput() {
       // 將驗證結果傳給父元件
@@ -71,24 +89,10 @@ export default {
     },
   },
   watch: {
-    // 將input值傳給父元件
+    // 監聽父元件傳入的inputValue
     inputValue: function (newValue, oldValue) {
-      // this.$emit('inputValue', this.inputVal)
+      // 確保驗證完成後才輸出output給父元件
       this.validate(newValue).then(this.sendOutput())
     },
   },
 }
-
-// <!-- 勾勾(fill) -->
-// <!-- <svg
-//             xmlns="http://www.w3.org/2000/svg"
-//             width="20"
-//             height="20"
-//             viewBox="0 0 20 20"
-//             fill="none"
-//           >
-//             <path
-//               d="M7.08333 13.8215L3.92259 10.6607C3.59715 10.3353 3.06951 10.3353 2.74408 10.6607C2.41864 10.9862 2.41864 11.5138 2.74408 11.8393L6.49408 15.5893C6.81951 15.9147 7.34715 15.9147 7.67259 15.5893L16.8393 6.42259C17.1647 6.09715 17.1647 5.56951 16.8393 5.24408C16.5138 4.91864 15.9862 4.91864 15.6607 5.24408L7.08333 13.8215Z"
-//               fill=""
-//             />
-//           </svg> -->
